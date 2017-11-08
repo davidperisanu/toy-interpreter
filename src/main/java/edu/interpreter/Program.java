d42815484f80295fae5d5a593c35afcc5daf834c
David Perisanu <david.perisanu@gmail.com>
2017-11-08 04:54:20 +0200

    Project initialization

---
+++ src/main/java/edu/interpreter/Program.java
@@ -0,0 +1,11 @@
+package edu.interpreter;
+
+/**
+ * The main entry point for the application.
+ * @author David Perisanu
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("All good.");
+    }
+}
